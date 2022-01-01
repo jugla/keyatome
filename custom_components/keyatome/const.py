f58ecb8382e535af68b574ed231c0fa0982d9ae6
jugla <jugla@free.fr>
2022-01-01 17:25:14 +0100

    slow down daily request

--- custom_components/keyatome/const.py
+++ custom_components/keyatome/const.py
@@ -34,7 +34,7 @@ ATTR_PERIOD_PRICE = "price"
 
 # Scan interval
 LIVE_SCAN_INTERVAL = timedelta(seconds=30)
-DAILY_SCAN_INTERVAL = timedelta(seconds=150)
+DAILY_SCAN_INTERVAL = timedelta(minutes=5)
 WEEKLY_SCAN_INTERVAL = timedelta(hours=1)
 MONTHLY_SCAN_INTERVAL = timedelta(hours=1)
 YEARLY_SCAN_INTERVAL = timedelta(days=1)

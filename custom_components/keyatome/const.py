4273014c43435adfb33d2c547467703d5273356b
jugla <jugla@free.fr>
2022-01-05 23:51:08 +0100

    avoid too many request at a time

--- custom_components/keyatome/const.py
+++ custom_components/keyatome/const.py
@@ -32,12 +32,12 @@ ATTR_PREVIOUS_PERIOD_USAGE = "previous_consumption"
 ATTR_PREVIOUS_PERIOD_PRICE = "previous_price"
 ATTR_PERIOD_PRICE = "price"
 
-# Scan interval
+# Scan interval (avoid synchronisation to lower request per seconds on server)
 LIVE_SCAN_INTERVAL = timedelta(seconds=30)
-DAILY_SCAN_INTERVAL = timedelta(minutes=5)
-WEEKLY_SCAN_INTERVAL = timedelta(hours=1)
-MONTHLY_SCAN_INTERVAL = timedelta(hours=1)
-YEARLY_SCAN_INTERVAL = timedelta(days=1)
+DAILY_SCAN_INTERVAL = timedelta(minutes=5, seconds=3)
+WEEKLY_SCAN_INTERVAL = timedelta(hours=1, seconds=5)
+MONTHLY_SCAN_INTERVAL = timedelta(hours=1, seconds=7)
+YEARLY_SCAN_INTERVAL = timedelta(days=1, seconds=11)
 
 # Type to call py key atome
 LIVE_TYPE = "live"
